49f61ee65b71841846840c6d43905b72915e94fb
Anderson-Lai <andersonmhlai26@gmail.com>
2024-08-07 15:30:18 -0400

    started taking in input

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
+#include <stdio.h>
+#include <string.h>
 
+#define BUFFER_SIZE 1024
 
 int main(int argc, char** argv) {
 
+    printf("This is an implementation of a Unix shell in C\n");
+    
+    char buffer[BUFFER_SIZE] = {};
+    
+    while (1) {
+        printf("> $ ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        
+        shell_parse_buffer(buffer);
+        
+        memset(buffer, '\0', BUFFER_SIZE);
+    }
 
     return 0;
 }
